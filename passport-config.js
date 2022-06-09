cd9f613fcb6395fbd138287fcbe62e8f0f223dd8
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-06-08 23:51:25 -0700

    finished with christian not connected to db

--- passport-config.js
+++ passport-config.js
@@ -1,26 +1,35 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport,getUserByEmail){
+function initialize(passport,getUserByEmail,getUserById){
     const authenticateUser = async (email,password, done) =>{
+        console.log("HER!");
         const user = getUserByEmail(email)
         if(user == null){
+            console.log("1");
             return done(null,false,{message:'No user with that email'})
         }
 
         try{
+            console.log("2");
             if(await bcrypt.compare(password, user.password)) {
-
+                return done(null,user)
             }else{
+                console.log("3");
                 return done(null,false,{message:'Password incorrect'})
             }
         }catch(e){
+            console.log("4");
             return done(e)
         }
     }
+    console.log("5");
     passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-    passport.serializeUser((user,done) => { })
-    passport.deserializeUser((id,done) => { })
+    passport.serializeUser((user,done) => done(null, user.id))
+    passport.deserializeUser((id,done) => {
+        console.log("6");
+        return done(null, getUserById(id))
+    })
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const PORT = 8000
+const methodOverride = require('method-override')
 const MongoClient = require('mongodb').MongoClient
 
 let db;
-
+/*
 console.log('connecting to DB...')
 MongoClient.connect('mongodb+srv://IsaacHHB:Hollowhorn7@cluster0.lkpys.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
 .then(client => {
@@ -24,11 +25,12 @@ MongoClient.connect('mongodb+srv://IsaacHHB:Hollowhorn7@cluster0.lkpys.mongodb.n
   })
 })
 console.log("DB connect has been kicked off...")
-
+*/
 const initializePassport = require('./passport-config')
 initializePassport(
     passport,
-    email => users.find(user => user.email === email)
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
 )
 
 const users = []
@@ -43,28 +45,30 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 
-app.get('/',(req,res)=>{
+app.get('/', checkAuthenticated, (req,res)=>{
     res.render('index.ejs',{name: 'Kyle'})
 })
 
-app.get('/login',(req,res)=>{
+app.get('/login',checkNotAuthenticated,(req,res)=>{
     res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirct: '/login',
     failureFlash: true
 }))
 
-app.get('/register',(req,res)=>{
+app.get('/register',checkNotAuthenticated, (req,res)=>{
     res.render('register.ejs')
 })
 
-app.post('/register',async (req,res)=>{
+app.post('/register',checkNotAuthenticated, async (req,res)=>{
     try{
+        /*
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         console.log("register POST received!");
         const usersCollection = db.collection('users')
@@ -78,8 +82,8 @@ app.post('/register',async (req,res)=>{
         .then(result => {
         res.render('regSuccess.ejs', {name: req.body.name})
         })
-
-        /*
+        */
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
@@ -89,7 +93,7 @@ app.post('/register',async (req,res)=>{
         //res.redirect('/login')
         let tmp_name = "Bob";
         res.render('regSuccess.ejs', {name: tmp_name});
-        */
+        
     }catch{
         res.redirect('/register')
     }
@@ -105,8 +109,33 @@ app.post('/users',(req,res)=>{
 
 })
 
-/*
+app.delete('/logout', (req,res)=>{
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req,res,next){
+    console.log("authenticatin?");
+    if (users.length > 0) {
+        console.log("users array size: " + users.length + ", user 0: " + users[0].email);
+    }
+    if(req.isAuthenticated()){
+        console.log(" is autheticated!");
+        return res.redirect('/')
+        
+    }
+    //console.log(" not authenticated....going to next: " + next);
+    next()
+}
+
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
-*/
